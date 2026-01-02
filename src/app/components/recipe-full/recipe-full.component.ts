a00248b6478f48fc3fd4ee0d113beed3211b4717
Oleksandr Shyrin <hs1985irin@gmail.com>
2026-01-02 23:14:31 +0000

    Add butons to Full Recipe page

--- src/app/components/recipe-full/recipe-full.component.ts
+++ src/app/components/recipe-full/recipe-full.component.ts
@@ -1,18 +1,24 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { RecipeModel } from 'src/app/models/recipe.model';
 import { IngredientComponent } from './ingredient/ingredient.component';
+import { IonButton, IonIcon } from '@ionic/angular/standalone';
+import { RouterModule } from '@angular/router';
+import { addIcons } from 'ionicons';
+import { heart } from 'ionicons/icons';
 
 @Component({
   selector: 'app-recipe-full',
   templateUrl: './recipe-full.component.html',
   styleUrls: ['./recipe-full.component.scss'],
-  imports: [IngredientComponent],
+  imports: [IngredientComponent, IonButton, RouterModule, IonIcon],
 })
 export class RecipeFullComponent implements OnInit {
   @Input()
   recipe!: RecipeModel;
 
-  constructor() {}
+  constructor() {
+    addIcons({ heart });
+  }
 
   ngOnInit() {}
 }
