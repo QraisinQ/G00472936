16fd0a8a87a28a0162aad517e93063d9337ee009
Oleksandr Shyrin <hs1985irin@gmail.com>
2026-01-02 16:14:08 +0000

    Add Recipe List component starter

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -6,12 +6,20 @@ import {
   IonContent,
 } from '@ionic/angular/standalone';
 import { SearchComponent } from '../components/search/search.component';
+import { RecipeListComponent } from '../components/recipe-list/recipe-list.component';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, SearchComponent],
+  imports: [
+    IonHeader,
+    IonToolbar,
+    IonTitle,
+    IonContent,
+    SearchComponent,
+    RecipeListComponent,
+  ],
 })
 export class HomePage {
   constructor() {}
