8dda1d22378357b3ca82c9100d5cb646f1919511
Oleksandr Shyrin <hs1985irin@gmail.com>
2026-01-02 23:43:54 +0000

    Add Favorite List store functionality

--- src/app/components/recipe-full/recipe-full.component.ts
+++ src/app/components/recipe-full/recipe-full.component.ts
@@ -5,6 +5,7 @@ import { IonButton, IonIcon } from '@ionic/angular/standalone';
 import { RouterModule } from '@angular/router';
 import { addIcons } from 'ionicons';
 import { heart } from 'ionicons/icons';
+import { LocalService } from 'src/app/services/local-service';
 
 @Component({
   selector: 'app-recipe-full',
@@ -13,12 +14,36 @@ import { heart } from 'ionicons/icons';
   imports: [IngredientComponent, IonButton, RouterModule, IonIcon],
 })
 export class RecipeFullComponent implements OnInit {
+  private favoriteList: RecipeModel[];
+  private FAVORITE = 'FAV';
+
   @Input()
   recipe!: RecipeModel;
 
-  constructor() {
+  constructor(private localService: LocalService) {
     addIcons({ heart });
+    const strData = this.localService.getElement(this.FAVORITE) ?? '[]';
+    this.favoriteList = JSON.parse(strData);
   }
 
   ngOnInit() {}
+
+  isFavorite(id: number) {
+    return this.favoriteList.some((r) => r.id === id);
+  }
+
+  addToFavorite() {
+    if (this.isFavorite(this.recipe.id)) {
+      this.favoriteList = this.favoriteList.filter(
+        (r) => r.id !== this.recipe.id
+      );
+    } else {
+      this.favoriteList.push(this.recipe);
+    }
+
+    this.localService.setElement(
+      this.FAVORITE,
+      JSON.stringify(this.favoriteList)
+    );
+  }
 }
