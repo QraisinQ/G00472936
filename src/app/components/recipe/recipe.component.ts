5b6a4066f160014e3ac8317c3899350c5cc3d47a
Oleksandr Shyrin <hs1985irin@gmail.com>
2026-01-02 22:33:42 +0000

    Add transition from recipe to details

--- src/app/components/recipe/recipe.component.ts
+++ src/app/components/recipe/recipe.component.ts
@@ -1,12 +1,13 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { RecipeModel } from 'src/app/models/recipe.model';
 import { IonButton, IonItemDivider } from '@ionic/angular/standalone';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-recipe',
   templateUrl: './recipe.component.html',
   styleUrls: ['./recipe.component.scss'],
-  imports: [IonButton, IonItemDivider],
+  imports: [IonButton, IonItemDivider, RouterModule],
 })
 export class RecipeComponent implements OnInit {
   @Input()
