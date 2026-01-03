e880e6d1cedcceabab4c1c4c0e4913a7a7ca7132
Oleksandr Shyrin <hs1985irin@gmail.com>
2026-01-03 00:19:23 +0000

    Add header routing buttons

--- src/app/components/app-header/app-header.component.ts
+++ src/app/components/app-header/app-header.component.ts
@@ -1,14 +1,25 @@
 import { Component, OnInit } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle } from '@ionic/angular/standalone';
+import {
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonButton,
+  IonIcon,
+} from '@ionic/angular/standalone';
+import { RouterModule } from '@angular/router';
+import { addIcons } from 'ionicons';
+import { heart, constructOutline } from 'ionicons/icons';
 
 @Component({
   selector: 'app-header',
   templateUrl: './app-header.component.html',
   styleUrls: ['./app-header.component.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle],
+  imports: [IonHeader, IonToolbar, IonTitle, IonButton, IonIcon, RouterModule],
 })
 export class AppHeaderComponent implements OnInit {
-  constructor() {}
+  constructor() {
+    addIcons({ heart, constructOutline });
+  }
 
   ngOnInit() {}
 }
--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,25 +1,19 @@
 import { Component } from '@angular/core';
-import {
-  IonHeader,
-  IonToolbar,
-  IonTitle,
-  IonContent,
-} from '@ionic/angular/standalone';
+import { IonContent } from '@ionic/angular/standalone';
 import { SearchComponent } from '../../components/search/search.component';
 import { RecipeListComponent } from '../../components/recipe-list/recipe-list.component';
 import { RecipeModel } from 'src/app/models/recipe.model';
+import { AppHeaderComponent } from 'src/app/components/app-header/app-header.component';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   imports: [
-    IonHeader,
-    IonToolbar,
-    IonTitle,
     IonContent,
     SearchComponent,
     RecipeListComponent,
+    AppHeaderComponent,
   ],
 })
 export class HomePage {
