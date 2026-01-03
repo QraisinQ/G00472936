2c63b2ee38a8744c119287db9919ac1c7e3db07b
Oleksandr Shyrin <hs1985irin@gmail.com>
2026-01-03 16:52:24 +0000

    Recipe detailes with measures

--- src/app/components/recipe-full/ingredient/ingredient.component.ts
+++ src/app/components/recipe-full/ingredient/ingredient.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Ingredient } from 'src/app/models/recipe.model';
+import { LocalService } from 'src/app/services/local-service';
 
 @Component({
   selector: 'app-ingredient',
@@ -7,10 +8,21 @@ import { Ingredient } from 'src/app/models/recipe.model';
   styleUrls: ['./ingredient.component.scss'],
 })
 export class IngredientComponent implements OnInit {
+  private MEASURE = 'MEAS';
+  measure: string = 'metric';
+  amount: string = '';
+  unit: string = '';
+
   @Input()
   ingredient!: Ingredient;
 
-  constructor() {}
+  constructor(private localService: LocalService) {
+    this.measure =
+      this.localService.getElement(this.MEASURE) === 'us' ? 'us' : 'metric';
+  }
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.amount = this.ingredient.measures[this.measure].amount;
+    this.unit = this.ingredient.measures[this.measure].unitLong;
+  }
 }
